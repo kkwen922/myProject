281e17336c8a3421456cb298416dcf719d5626d0
kkwen922 <kkwen922@yahoo.com.tw>
2023-01-04 14:14:38 +0800

    所有的类都必须添加创建者信息。

--- src/main/java/my/project/modules/notify/sendNotifyController.java
+++ src/main/java/my/project/modules/notify/sendNotifyController.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author kevinchang
+ */
 @Controller
 @RequestMapping("/notify")
 @Slf4j
